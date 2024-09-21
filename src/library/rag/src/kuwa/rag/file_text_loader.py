f4440de2c294c59a6dac22a911ae3d96a5cde5ff
Yung-Hsiang Hu <iftnt1999@gmail.com>
2024-09-21 20:27:16 +0800

    [lib] add: RAG suporting "text/" MIME type

--- src/library/rag/src/kuwa/rag/file_text_loader.py
+++ src/library/rag/src/kuwa/rag/file_text_loader.py
@@ -1,16 +1,29 @@
 import logging
 import asyncio
-from typing import List, Optional, AsyncIterator
-from pathlib import Path
-
-from langchain.docstore.document import Document
-from langchain_community.document_loaders.text import TextLoader
 import trafilatura
 import textract
+from typing import List, Optional, AsyncIterator
+from pathlib import Path
+from langchain.docstore.document import Document
+from langchain_community.document_loaders.text import TextLoader
 from magika import Magika
 
 logger = logging.getLogger(__name__)
 magika = Magika()
+
+class PrefixDict(dict):
+    def __missing__(self, key):
+        """
+        Longest prefix match the key if not exact match.
+        """
+        prefix_dict = {k: v for k, v in self.items() if key.startswith(k)}
+        match len(prefix_dict):
+            case 0:
+                return None
+            case 1:
+                return list(prefix_dict.values())[0]
+            case _:
+                return prefix_dict[max(prefix_dict.keys(), key=len)]
 
 class FileTextLoader(TextLoader):
     """Extract text from files with textract or Trafilatura.
@@ -29,20 +42,31 @@ class FileTextLoader(TextLoader):
         
         file_magic_result = magika.identify_path(Path(self.file_path))
         file_mime_type = file_magic_result.output.mime_type.split(';', 1)[0]
-        logger.debug(f"{self.file_path}: {file_mime_type}")
         
-        mime_extractor = {
+        mime_extractors = PrefixDict(**{
+            'text/': self.load_plain_text,
+            'application/x-yaml': self.load_plain_text,
+            'application/yaml': self.load_plain_text,
+            'application/json': self.load_plain_text,
             'text/html': self.load_html,
             'multipart/related': self.load_html,
             'application/xhtml+xml': self.load_html,
-        }
+        })
         fallback_extractor = self.load_doc
 
-        extractor = fallback_extractor
-        if file_mime_type in mime_extractor:
-            extractor = mime_extractor[file_mime_type]
+        extractor = mime_extractors[file_mime_type]
+        if extractor is None:
+            extractor = fallback_extractor
         
-        return extractor()
+        logger.debug(f"{self.file_path}: {file_mime_type}, {extractor.__name__}")
+
+        try:
+            docs = extractor()
+        except Exception as e:
+            logger.warning(f"Error loading {self.file_path}: {type(e).__name__}: {e.args[0]}. Skipped.")
+            return []
+        finally:
+            return docs
     
     async def alazy_load(
         self,
@@ -51,16 +75,20 @@ class FileTextLoader(TextLoader):
         docs = await loop.run_in_executor(None, self.lazy_load)
         for doc in docs:
             yield doc
-    
+
+    def load_plain_text(self) -> List[Document]:
+        """Load text from plaintext file."""
+        content = ""
+        with open(self.file_path, encoding=self.encoding, errors='ignore') as f:
+            content = f.read()
+        
+        metadata = {"source": self.file_path}
+        return [Document(page_content=content, metadata=metadata)]
+
     def load_doc(self) -> List[Document]:
         """Load text from document file."""
-        text = ""
-        try:
-            text = textract.process(self.file_path, encoding=self.encoding)
-            text = text.decode(self.encoding)
-        except Exception as e:
-            logger.warning(f"Error loading {self.file_path}. Skipped.")
-            return []
+        text = textract.process(self.file_path, encoding=self.encoding)
+        text = text.decode(self.encoding)
 
         metadata = {"source": self.file_path}
         return [Document(page_content=text, metadata=metadata)]
@@ -68,20 +96,17 @@ class FileTextLoader(TextLoader):
     def load_html(self) -> List[Document]:
         """Load text from HTML file."""
         text = ""
-        try:
-            config = trafilatura.settings.use_config()
-            config.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
-            
-            content = ""
-            with open(self.file_path, encoding=self.encoding, errors='ignore') as f:
-                content = f.read()
-            text = trafilatura.extract(
-                content,
-                favor_precision=True,
-                config=config,
-            )
-        except Exception as e:
-            raise RuntimeError(f"Error loading {self.file_path}") from e
+        config = trafilatura.settings.use_config()
+        config.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
+        
+        content = ""
+        with open(self.file_path, encoding=self.encoding, errors='ignore') as f:
+            content = f.read()
+        text = trafilatura.extract(
+            content,
+            favor_precision=True,
+            config=config,
+        )
 
         metadata = {"source": self.file_path}
         return [Document(page_content=text, metadata=metadata)]
