de67263587f98806bbf8f3a5aa7a7d0b1f3b0b20
Yung-Hsiang Hu <iftnt1999@gmail.com>
2024-10-15 16:32:26 +0800

    [script] fix: supervisor format

--- scripts/linux/generate_supervisor_conf.py
+++ scripts/linux/generate_supervisor_conf.py
@@ -5,13 +5,13 @@ from jinja2 import Environment, Template
 conf_data_file = "supervisor_conf.yaml"
 
 # Define the template string
-conf_template = """{% for item in program %}[program:ke-{{item['access_code']}}]
+conf_template = """{% for item in program %}[program:{{item['access_code']}}]
 command={{cmd_prefix|safe}}{{item['cmd']|safe}} --access_code {{item['access_code']}} {{cmd_suffix|safe}}
 directory={{item['working_dir']|default(working_dir)}}
 environment={% if env is defined and env is not none %}{% for key, value in env.items() %}{{key}}={{value|safe}}{{ "," if not loop.last else "" }}{% endfor %}{% endif %}
 numprocs={{item['num_procs']|default('1')}}
 {% if user is defined %}user={{user}}{% endif %}
-process_name=%(program_name)s_%(process_num)s
+process_name=%(program_name)s_%(process_num)02d
 stopsignal=INT
 redirect_stderr=true
 
