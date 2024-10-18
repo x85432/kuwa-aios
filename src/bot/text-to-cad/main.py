e39591282ce15035a09f77171a3228c7042f46c4
Yung-Hsiang Hu <iftnt1999@gmail.com>
2024-10-18 20:20:05 +0800

    [cad] add: translate and generate button

--- src/bot/text-to-cad/main.py
+++ src/bot/text-to-cad/main.py
@@ -1,7 +1,13 @@
+import os
 import logging
 import gradio as gr
+from pathlib import Path
 from dotenv import load_dotenv
+from gradio_i18n import Translate, gettext as _
 from src.text_to_cad import text_to_cad
+
+def clear_all():
+    return [None, None, gr.DownloadButton(visible=False)]
 
 def translate_prompt(prompt, language):
     # Your prompt translation logic here
@@ -11,34 +17,44 @@ def translate_prompt(prompt, language):
     # return translated_prompt
     return prompt
 
+def generate_cad_model(prompt):
+    file_path = text_to_cad(prompt=prompt)
+    name = Path(file_path).name
+    yield [
+        file_path,
+        gr.DownloadButton(label=_("Download")+f" {name}", value=file_path, visible=True)
+    ]
+    os.remove(file_path)
+
+
+def create_main_ui():
+    gr.Markdown(_("# Text to CAD Model Generator"))
+    with gr.Row(equal_height=True):
+        with gr.Column(scale=1, min_width=300):
+            prompt_area = gr.TextArea(label=_("Prompt"))
+            with gr.Row():
+                clear_btn = gr.Button(_("Clear"))
+                translate_btn = gr.Button(_("Translate"), interactive=False)
+                generate_btn = gr.Button(_("Generate"), variant="primary")
+        with gr.Column(scale=2, min_width=300):
+            model_display = gr.Model3D(label=_("3D Model"))
+            download_btn = gr.DownloadButton(visible=False)
+    gr.Examples(label=_("Examples"), examples=[
+        _("A dodecahedron"), _("A 1/2 inch gear with 21 teeth"), _("Design a gear with 40 teeth"), _("A 3x6 lego"), _("一個60齒的齒輪")
+    ], inputs=prompt_area)
+
+    clear_btn.click(clear_all, None, [prompt_area, model_display, download_btn])
+    translate_btn.click(translate_prompt, prompt_area, prompt_area)
+    generate_btn.click(generate_cad_model, prompt_area, [model_display, download_btn])
+
 if __name__ == "__main__":
     load_dotenv()  # take environment variables from .env.
     logging.basicConfig(level=logging.INFO)
 
     # Create the Gradio interface
-    iface = gr.Interface(
-        fn=text_to_cad,
-        inputs=gr.TextArea(label="Prompt"),
-        outputs=gr.Model3D(label="3D Model"),
-        title="Text to CAD Model Generator",
-        examples=["A dodecahedron", "A 1/2 inch gear with 21 teeth", "Design a gear with 40 teeth", "A 3x6 lego"],
-        flagging_mode="never",
-        theme=gr.themes.Soft(),
-    )
+    with gr.Blocks(theme=gr.themes.Soft()) as ui:
+        lang = gr.Dropdown(choices=[("English", "en"), ("中文", "zh")], label=_("Language"))
+        with Translate("translation.yaml", lang, placeholder_langs=["en", "zh"]):
+            create_main_ui()
 
-    iface.launch(share=True, server_name='0.0.0.0')
-
-    # # Add a "Translate" button with a dropdown for language selection
-    # with iface:
-    #     with gr.Row():
-    #         language_dropdown = gr.Dropdown(label="Language", choices=["English", "Spanish", "French", "German"], value="English")
-    #         translate_button = gr.Button("Translate")
-
-    #     translate_button.click(
-    #         fn=translate_prompt,
-    #         inputs=[iface.input, language_dropdown],
-    #         outputs=[iface.input]
-    #     )
-
-    # # Start the interface
-    # iface.launch()
+    ui.launch(share=False, server_name='0.0.0.0')
