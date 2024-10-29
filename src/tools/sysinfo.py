ef9f9906030c563dd124f39c54373960a8357bde
Yung-Hsiang Hu <iftnt1999@gmail.com>
2024-10-29 08:47:16 +0800

    [tool] add: env and kernel info to sysinfo

--- src/tools/sysinfo.py
+++ src/tools/sysinfo.py
@@ -1,6 +1,9 @@
 #!/usr/local/bin/python
 
+import os
 import platform
+import requests
+from urllib.parse import urljoin
 from importlib.metadata import version
 
 def get_kuwa_info():
@@ -30,6 +33,41 @@ def get_torch_info():
         ]
     return result
 
+def get_env_var():
+    result = ["**Environment Variables**"]
+    env = dict(os.environ)
+    env["KUWA_API_KEY"] = "******"
+    env = dict(sorted(env.items()))
+    result.extend([f'{k}={v}' for k,v in env.items()])
+
+    return result
+
+def get_kernel_info():
+    result = ["**Registered Executors**"]
+    kernel_debug_url = urljoin(os.environ.get("KUWA_KERNEL_BASE_URL"), "./worker/debug")
+
+    try:
+        resp = requests.get(kernel_debug_url, headers={"Accept":"application/json"})
+        resp.raise_for_status()
+        registered_executors = resp.json()
+        result.extend([
+            "| Access Code (ID) | Endpoint | Status | Job History ID | Job User ID |",
+            "|---|---|---|---|---|",
+        ])
+        for access_code, group in registered_executors.items():
+            result.extend([
+                "|{access_code}|{endpoint}|{status}|{job_history_id}|{job_user_id}|".format(
+                    access_code=access_code,
+                    **executor,
+                )
+                for executor in group
+            ])
+    except Exception as e:
+        result.append(f"Failed to get registered executors form kernel: {repr(e)}")
+    finally:
+        return result
+
+
 if __name__ == "__main__":
-    sysinfo = get_kuwa_info() + [''] + get_torch_info()
-    print('\n'.join(sysinfo))
+    sysinfo = [get_kuwa_info(), get_torch_info(), get_env_var(), get_kernel_info()]
+    print('\n\n'.join(['\n'.join(section) for section in sysinfo]))
