6713b500bc5a27ef73c8e69903fa4d72f33aaec9
Yung-Hsiang Huv <shaun.hu@thuniverse.com>
2025-03-20 04:29:52 +0800

    [win] fix: Gemma3's HF model name

--- windows/executors/gemma3-1b/_run.bat
+++ windows/executors/gemma3-1b/_run.bat
@@ -1,8 +1,8 @@
 pushd ..\..\src
 call variables.bat
 popd
-set EXECUTOR_ACCESS_CODE=.model/google/gemma3-1b-it
+set EXECUTOR_ACCESS_CODE=.model/google/gemma-3-1b-it
 pushd ..\..\..\src\multi-chat
-php artisan model:config ".model/google/gemma3-1b-it" "Gemma3-1B" --image "..\..\windows\executors\gemma3-1b\gemma3-1b.png"
+php artisan model:config ".model/google/gemma-3-1b-it" "Gemma3-1B" --image "..\..\windows\executors\gemma3-1b\gemma-3-1b.png"
 popd
-start /b "" "kuwa-executor" "huggingface" "--access_code" ".model/google/gemma3-1b-it" "--model_path" "google/gemma3-1b-it" --log debug
+start /b "" "kuwa-executor" "huggingface" "--access_code" ".model/google/gemma-3-1b-it" "--model_path" "google/gemma-3-1b-it" --log debug
--- windows/executors/gemma3-4b/_run.bat
+++ windows/executors/gemma3-4b/_run.bat
@@ -1,8 +1,8 @@
 pushd ..\..\src
 call variables.bat
 popd
-set EXECUTOR_ACCESS_CODE=.model/google/gemma3-4b-it
+set EXECUTOR_ACCESS_CODE=.model/google/gemma-3-4b-it
 pushd ..\..\..\src\multi-chat
-php artisan model:config ".model/google/gemma3-4b-it" "Gemma3-4B" --image "..\..\windows\executors\gemma3-4b\gemma3-4b.png"
+php artisan model:config ".model/google/gemma-3-4b-it" "Gemma3-4B" --image "..\..\windows\executors\gemma3-4b\gemma-3-4b.png"
 popd
-start /b "" "kuwa-executor" "huggingface" "--access_code" ".model/google/gemma3-4b-it" "--model_path" "google/gemma3-4b-it" --log debug
+start /b "" "kuwa-executor" "huggingface" "--access_code" ".model/google/gemma-3-4b-it" "--model_path" "google/gemma-3-4b-it" --log debug
