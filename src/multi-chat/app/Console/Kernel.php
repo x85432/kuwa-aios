da62d7fe74140b0de2a4b2a91ff769df9eb0f996
Drift <taifu9920923@gmail.com>
2025-03-27 16:36:59 +0800

    Fix migration missing import

--- src/multi-chat/app/Console/Kernel.php
+++ src/multi-chat/app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs\CheckUpdate;
+use App\Jobs\HealthCheck;
 
 class Kernel extends ConsoleKernel
 {
