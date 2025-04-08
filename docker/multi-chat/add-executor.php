65ea8acb60a64014b416d8df2937673c1803b8f7
Yung-Hsiang Hu <iftnt1999@gmail.com>
2025-04-08 18:49:52 +0800

    [docker] add: Groq Executor

--- docker/multi-chat/add-executor.php
+++ docker/multi-chat/add-executor.php
@@ -5,12 +5,17 @@
 
 $access_code = escapeshellarg($_POST["access_code"]);
 $name = escapeshellarg($_POST["name"]);
+$create_bot = escapeshellarg($_POST["create_bot"]);
 $image = "";
 if (isset($_POST["image"])){
     $image = '/app/public/images/' . $_POST["image"];
     $image = "--image=".escapeshellarg($image);
 }
+$do_not_create_bot = ""
+if (isset($_POST["create_bot"]) && (strtolower($_POST["create_bot"]) == "false")) {
+  $do_not_create_bot = "--do_not_create_bot";
+} 
 
-exec(sprintf("php /app/artisan model:config %s %s %s 2>&1", $access_code, $name, $image), $output, $retval);
+exec(sprintf("php /app/artisan model:config %s %s %s %s 2>&1", $access_code, $name, $image, $do_not_create_bot), $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
