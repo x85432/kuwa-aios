b42e3e5912b7f5b564300f6ba0b1c1ab47485d93
Drift <taifu9920923@gmail.com>
2025-05-02 18:29:20 +0800

    [win] remove extra "i" inside CopyCat name

--- windows/executors/copycat/run.bat
+++ windows/executors/copycat/run.bat
@@ -1,5 +1,5 @@
 set EXECUTOR_ACCESS_CODE=.tool/kuwa/copycat
 pushd ..\..\..\src\multi-chat
-php artisan model:config ".tool/kuwa/copycat" "CopyCati" --order=999000 --image "..\..\windows\executors\copycat\copycat.png"
+php artisan model:config ".tool/kuwa/copycat" "CopyCat" --order=999000 --image "..\..\windows\executors\copycat\copycat.png"
 popd
 start /b "" "kuwa-executor" "debug" "--access_code" ".tool/kuwa/copycat"
