016bd0cb6af5c87862e57c8c5b1931a207570ecd
Drift <taifu9920923@gmail.com>
2025-05-06 20:47:53 +0800

    [multi-chat] Update store button design

--- src/multi-chat/resources/views/store.blade.php
+++ src/multi-chat/resources/views/store.blade.php
@@ -22,14 +22,14 @@
             @if (request()->user()->hasPerm(['tab_Manage', 'Store_create_community_bot', 'Store_create_group_bot', 'Store_create_private_bot']))
                 <div class="create-bot-btn mb-2 mt-8 mx-auto h-[50px] flex">
                     <a href="https://repository.kuwaai.org" target="_blank"
-                        class="flex menu-btn flex items-center p-3 justify-center text-green-500 h-12 dark:hover:bg-gray-700 border border-green-500 border-1 rounded-l-lg hover:bg-gray-300 transition duration-300 overflow-hidden"><i
+                        class="flex menu-btn flex items-center px-4 py-3 justify-center text-green-500 h-12 dark:hover:bg-gray-700 border border-green-500 border-1 rounded-l-lg hover:bg-gray-300 transition duration-300 overflow-hidden"><i
                             class="fas fa-globe"></i></a>
                     <button data-modal-target="create-bot-modal" data-modal-toggle="create-bot-modal"
-                        class="flex menu-btn flex items-center justify-center w-auto px-3 h-12 dark:hover:bg-gray-700 border border-green-500 border-1 hover:bg-gray-300 transition duration-300 overflow-hidden">
-                        <p class="flex-1 text-center text-green-500">{{ __('store.button.create') }}</p>
+                        class="bg-green-500 hover:bg-green-600 flex menu-btn flex items-center justify-center w-auto px-3 h-12 transition duration-300 overflow-hidden">
+                        <p class="flex-1 text-center text-white">{{ __('store.button.create') }}</p>
                     </button>
                     <label for="upload_bot_modelfile"
-                        class="bg-green-500 hover:bg-green-600 cursor-pointer h-12 text-white font-bold py-3 px-4 rounded-r-lg  flex items-center justify-center transition duration-300">
+                        class="text-green-500 cursor-pointer h-12 font-bold py-3 px-4 rounded-r-lg flex items-center dark:hover:bg-gray-700 border border-green-500 border-1 justify-center hover:bg-gray-300 transition duration-300">
                         <i class="fas fa-file-import"></i>
                     </label>
                     <input type="file" accept=".bot" class="hidden" id="upload_bot_modelfile"
