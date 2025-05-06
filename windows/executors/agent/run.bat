8d57f54892461a575f295662047691f14898fedc
Yung-Hsiang Hu <iftnt1999@gmail.com>
2025-05-06 22:37:47 +0800

    [win] fix: add more agent executor to prevent deadlock

--- windows/executors/agent/run.bat
+++ windows/executors/agent/run.bat
@@ -6,5 +6,7 @@ pushd ..\..\..\src\multi-chat
 php artisan model:config ".tool/kuwa/agent" "Agent" --order=999010 --image "..\..\windows\executors\agent\agent.png"
 popd
 pushd ..\..\..\src\executor\agent
+for /L %%a in (1,1,5) do (
 start /b "" "python" main.py "--access_code" ".tool/kuwa/agent" "--log" "debug"
+)
 popd
