6450826eae96387df1492c7379332da8db78a7e2
Yung-Hsiang Hu <iftnt1999@gmail.com>
2025-05-13 05:57:30 +0800

    [win] add: ollama bot

--- windows/executors/geminipro/_run.bat
+++
@@ -1,8 +0,0 @@
-pushd ..\..\src
-call variables.bat
-popd
-set EXECUTOR_ACCESS_CODE=.model:google/gemini
-pushd ..\..\..\src\multi-chat
-php artisan model:config ".model:google/gemini" "Gemini" --image "..\..\windows\executors\geminipro\geminipro.png" --order "401000"
-popd
-start /b "" "kuwa-executor" "geminipro" "--access_code" ".model:google/gemini"
