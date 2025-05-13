046855b47a3d00ae3b1bd4defbb3a30de6df48e4
Yung-Hsiang Hu <iftnt1999@gmail.com>
2025-05-13 14:26:35 +0800

    [win] fix: align bot and executor

--- windows/executors/gemma3-4b/_run.bat
+++ windows/executors/gemma3-4b/_run.bat
@@ -1,8 +1,8 @@
 pushd ..\..\src
 call variables.bat
 popd
-set EXECUTOR_ACCESS_CODE=.model/google/gemma
+set EXECUTOR_ACCESS_CODE=.model/google/gemma-3-4b
 pushd ..\..\..\src\multi-chat
-php artisan model:config ".model/google/gemma" "Gemma3-4B" --image "..\..\windows\executors\gemma3-4b\gemma-3-4b.png" "--order" "301000"
+php artisan model:config ".model/google/gemma-3-4b" "Gemma3-4B" --image "..\..\windows\executors\gemma3-4b\gemma-3-4b.png" "--order" "301000"
 popd
-start /b "" "kuwa-executor" "huggingface" "--access_code" ".model/google/gemma" "--model_path" "tetf/gemma-3-4b-it" "--log" "debug"
+start /b "" "kuwa-executor" "huggingface" "--access_code" ".model/google/gemma-3-4b" "--model_path" "tetf/gemma-3-4b-it" "--log" "debug"
