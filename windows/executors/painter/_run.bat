bf952a59e78af753d1980f7707b96b29793c85e0
Drift <taifu9920923@gmail.com>
2025-05-18 21:15:48 +0800

    [win] Fix painter and rag order

--- windows/executors/painter/_run.bat
+++ windows/executors/painter/_run.bat
@@ -3,7 +3,7 @@ call variables.bat
 popd
 set EXECUTOR_ACCESS_CODE=.model:diffusers/
 pushd ..\..\..\src\multi-chat
-php artisan model:config ".model:diffusers/" "Diffusers" --image "..\..\windows\executors\painter\painter.png" --order "120000"
+php artisan model:config ".model:diffusers/" "Diffusers" --image "..\..\windows\executors\painter\painter.png" --order "321000"
 popd
 pushd ..\..\..\src\executor\image_generation\
 start /b "" "python" main.py "--access_code" ".model:diffusers/"
