3e2f048e7c88c3ad11df8ddf131136df7fc08f0f
Drift <taifu9920923@gmail.com>
2025-05-20 21:08:07 +0800

    [multi-chat] Fix phi4 executor

--- windows/executors/phi4/_run.bat
+++ windows/executors/phi4/_run.bat
@@ -5,4 +5,4 @@ set EXECUTOR_ACCESS_CODE=.model/microsoft/phi-4-multimodal
 pushd ..\..\..\src\multi-chat
 php artisan model:config ".model/microsoft/phi-4-multimodal" "Phi 4 Multimodal" --image "..\..\windows\executors\phi4\phi4.png" "--order" "301100"
 popd
-start /b "" "kuwa-executor" "huggingface" "--access_code" ".model/microsoft/phi-4-multimodal" "--model_path" "." "--log" "debug"
+start /b "" "kuwa-executor" "huggingface" "--access_code" ".model/microsoft/phi-4-multimodal" "--model_path" "." "--log" "debug" "--trust_remote_code"
