3f245d991b5832ade1bbf15782db12df7a0e2d9b
Drift <taifu9920923@gmail.com>
2025-06-07 21:58:36 +0800

    [multi-chat] Production update message now support ANSI

--- src/multi-chat/executables/bat/production_update.bat
+++ src/multi-chat/executables/bat/production_update.bat
@@ -1,20 +1,28 @@
 cd ../..
-call composer install --no-dev --optimize-autoloader --no-interaction
-call php artisan key:generate
-call php artisan db:seed --class=InitSeeder --force
-call php artisan migrate --force
+
+call composer install --no-dev --optimize-autoloader --no-interaction --ansi
+
+call php artisan key:generate --ansi
+call php artisan db:seed --class=InitSeeder --force --ansi
+call php artisan migrate --force --ansi
+
 rmdir /Q /S public\storage
 rmdir /Q /S storage\app\public\root\custom
 rmdir /Q /S storage\app\public\root\database
 rmdir /Q /S storage\app\public\root\bin
 rmdir /Q /S storage\app\public\root\bot
-call php artisan storage:link
-call npm audit fix
-call npm install
-call npm audit fix
-call npm ci --no-audit --no-progress
-call php artisan route:cache
-call php artisan view:cache
-call php artisan optimize
-call npm run build
-call php artisan config:cache
+
+call php artisan storage:link --ansi
+
+call npm audit fix --loglevel verbose
+call npm install --color=always
+call npm audit fix --loglevel verbose
+call npm ci --no-audit --no-progress --color=always
+
+call php artisan route:cache --ansi
+call php artisan view:cache --ansi
+call php artisan optimize --ansi
+
+call npm run build --color=always
+
+call php artisan config:cache --ansi
